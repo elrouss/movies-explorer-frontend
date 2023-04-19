47ad7561b25ec53a14b446657f24975c3f5d9c79
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-19 23:45:14 +0300

    feat: сверстать html компонента AboutMe

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import avatar from "../../assets/images/img-avatar.jpg";
+
+function AboutMe() {
+  function calculateAge() {
+    const today = new Date();
+    const birthDate = new Date(1994, 2, 30);
+    const month = today.getMonth() - birthDate.getMonth();
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+
+    let years = String(age);
+    if (years >= 11 && years <= 14) {
+      years = 'лет';
+    } else if (years.endsWith('1')) {
+      years = 'год';
+    } else if (years.endsWith('2') || years.endsWith('3') || years.endsWith('4')) {
+      years = 'года';
+    } else {
+      years = 'лет';
+    }
+
+    return `${age} ${years}`;
+  }
+
+  return (
+    <section className="about-me">
+      <div className="wrapper">
+        <h2>Студент</h2>
+        <div className="about-me__wrapper">
+          <div className="about-me__biography">
+            <h3>Борис</h3>
+            <p>Фронтенд-разработчик, {calculateAge()}</p>
+            <p>Я живу в Санкт-Петербурге, окончил исторический факультет СПбГУ. С 2022 года я увлекся программированием. Прошёл курс по веб-разработке Яндекс Практикума, где прохожу сейчас специализированный курс по React, осваиваю также курс &laquo;JavaScript/Front-end&raquo; сообщества The Rolling Scopes.</p>
+            <a href="https://github.com/elrouss" rel="noreferrer" target="_blank">Github</a>
+          </div>
+          <img className="avatar" src={avatar} alt="Борис Зашляпин, веб-разработчик, молодой человек в очках с оранжевыми стеклами, с вьющимися волосами и приоткрытым ртом, в белой футболке и накинутом сверху розовом худи" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutMe;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import React from "react";
 import Promo from "../Promo/Promo.js";
 import AboutProject from "../AboutProject/AboutProject.js";
 import Techs from "../Techs/Techs.js";
+import AboutMe from "../AboutMe/AboutMe.js";
 
 export default function Main() {
   return (
@@ -10,6 +11,7 @@ export default function Main() {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </main>
   )
 }
