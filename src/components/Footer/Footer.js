93049c19d07a00cdc5d94c9ef7b5fb1eae9309f6
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-20 21:23:36 +0300

    feat: подключить стили к Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,26 +3,30 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="wrapper">
-        <h2>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-        <div className="footer__wrapper">
-          <span>&copy; {new Date().getFullYear()}</span>
-          <div className="footer__links">
-            <a
-              href="https://practicum.yandex.ru/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Яндекс.Практикум
-            </a>
-            <a
-              href="https://github.com/elrouss"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Github
-            </a>
-          </div>
+      <div className="wrapper footer__wrapper">
+        <h2 className="footer__heading">
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </h2>
+        <span className="footer__copyright">
+          &copy; {new Date().getFullYear()}
+        </span>
+        <div className="footer__links">
+          <a
+            className="link footer__link"
+            href="https://practicum.yandex.ru/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Яндекс.Практикум
+          </a>
+          <a
+            className="link footer__link"
+            href="https://github.com/elrouss"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Github
+          </a>
         </div>
       </div>
     </footer>
