ef54b0e805ac5f2abda8fe68014d358aee687303
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-24 21:50:15 +0300

    feat: сверстать гамбургер-меню

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useMatch } from "react-router-dom";
 
+import useWindowDimensions from "../../hooks/useWindowDimensions.js";
+
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation.js";
 
@@ -8,22 +10,44 @@ function Header() {
   const href = useMatch({ path: `${window.location.pathname}`, end: false });
   const isRootHref = href.pathnameBase === "/";
 
+  const windowWidth = useWindowDimensions();
+
+  function renderHeaderMenu() {
+    if (!isRootHref && windowWidth <= 768) {
+      return (
+        <button
+          className="btn hamburger"
+          type="button"
+          aria-label="Гамбургер-меню с навигацией по приложению"
+        >
+          <span className="hamburger__line"></span>
+          <span className="hamburger__line"></span>
+          <span className="hamburger__line"></span>
+        </button>
+      );
+    }
+
+    if (isRootHref) {
+      return (
+        <div className="header__auth">
+          <Link className="link" to={"/signup"}>
+            Регистрация
+          </Link>
+          <Link className="link link_color_accent btn-auth" to={"/signin"}>
+            Войти
+          </Link>
+        </div>
+      );
+    }
+
+    return <Navigation />;
+  }
+
   return (
     <header className="header">
       <div className="wrapper header__wrapper">
         <Logo />
-        {isRootHref ? (
-          <div className="header__auth">
-            <Link className="link" to={"/signup"}>
-              Регистрация
-            </Link>
-            <Link className="link link_color_accent btn-auth" to={"/signin"}>
-              Войти
-            </Link>
-          </div>
-        ) : (
-          <Navigation />
-        )}
+        {renderHeaderMenu()}
       </div>
     </header>
   );
