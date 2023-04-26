f42ed0baaf11eeb9b358f7b288edca0a77552a99
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-26 22:01:46 +0300

    refactor: убрать скопилированный css файл и изменить пути к шрифтам и иконкам

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './sass/index.css';
+import './sass/index.scss'
 
 import App from './components/App/App.js';
 
